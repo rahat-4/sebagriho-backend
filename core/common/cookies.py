bc64e57b01abc03049a9f9f963e9a7e7825872f9
rahat-4 <mainul.rahat4@gmail.com>
2025-09-08 21:56:09 +0600

    Update cookies settings

--- core/common/cookies.py
+++ core/common/cookies.py
@@ -1,5 +1,4 @@
-from datetime import timedelta
-
+from datetime import datetime, timedelta
 from django.conf import settings
 
 
@@ -7,34 +6,37 @@ def set_auth_cookies(response, access_token, refresh_token, remember_me=False):
     """
     Sets JWT cookies (access + refresh) and optional remember_me flag.
     """
-    # Set access token cookie
-    response.set_cookie(
-        key=settings.SIMPLE_JWT["AUTH_COOKIE"],
-        value=access_token,
-        expires=settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"],
-        secure=settings.SIMPLE_JWT["AUTH_COOKIE_SECURE"],
-        httponly=settings.SIMPLE_JWT["AUTH_COOKIE_HTTP_ONLY"],
-        samesite=settings.SIMPLE_JWT["AUTH_COOKIE_SAMESITE"],
-    )
+    # Calculate expiry times as datetime objects
+    access_expires = datetime.now() + settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"]
 
     if remember_me:
-        refresh_exp = timedelta(days=7)  # instead of 1 day
+        refresh_expires = datetime.now() + timedelta(days=7)
         response.set_cookie(
             key="remember_me",
             value="true",
-            expires=refresh_exp,
+            expires=refresh_expires,
             secure=settings.SIMPLE_JWT["AUTH_COOKIE_SECURE"],
             httponly=False,
             samesite=settings.SIMPLE_JWT["AUTH_COOKIE_SAMESITE"],
         )
     else:
-        refresh_exp = settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"]
+        refresh_expires = datetime.now() + settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"]
+
+    # Set access token cookie
+    response.set_cookie(
+        key=settings.SIMPLE_JWT["AUTH_COOKIE"],
+        value=access_token,
+        expires=access_expires,
+        secure=settings.SIMPLE_JWT["AUTH_COOKIE_SECURE"],
+        httponly=settings.SIMPLE_JWT["AUTH_COOKIE_HTTP_ONLY"],
+        samesite=settings.SIMPLE_JWT["AUTH_COOKIE_SAMESITE"],
+    )
 
     # Set refresh token cookie
     response.set_cookie(
         key="refresh_token",
         value=refresh_token,
-        expires=refresh_exp,
+        expires=refresh_expires,
         secure=settings.SIMPLE_JWT["AUTH_COOKIE_SECURE"],
         httponly=settings.SIMPLE_JWT["AUTH_COOKIE_HTTP_ONLY"],
         samesite=settings.SIMPLE_JWT["AUTH_COOKIE_SAMESITE"],
