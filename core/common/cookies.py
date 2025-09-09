ed7c5dee57b514641f5b187edfa9738ab2175cd3
rahat-4 <mainul.rahat4@gmail.com>
2025-09-09 14:14:46 +0600

    Update set cookies function

--- core/common/cookies.py
+++ core/common/cookies.py
@@ -7,10 +7,10 @@ def set_auth_cookies(response, access_token, refresh_token, remember_me=False):
     Sets JWT cookies (access + refresh) and optional remember_me flag.
     """
     # Calculate expiry times as datetime objects
-    access_expires = datetime.now() + settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"]
+    access_expires = settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"]
 
     if remember_me:
-        refresh_expires = datetime.now() + timedelta(days=7)
+        refresh_expires = timedelta(days=7)
         response.set_cookie(
             key="remember_me",
             value="true",
@@ -20,7 +20,7 @@ def set_auth_cookies(response, access_token, refresh_token, remember_me=False):
             samesite=settings.SIMPLE_JWT["AUTH_COOKIE_SAMESITE"],
         )
     else:
-        refresh_expires = datetime.now() + settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"]
+        refresh_expires = settings.SIMPLE_JWT["REFRESH_TOKEN_LIFETIME"]
 
     # Set access token cookie
     response.set_cookie(
